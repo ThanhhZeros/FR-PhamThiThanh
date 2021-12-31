d89a922dc96ad3f1e29252eb590ca2d7e4a5e9bb
ThanhhZeros <phamthanhnd00@gmail.com>
2021-12-31 13:14:09 +0700

    create solution and project first

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nNhap name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " !!");
+        }
+    }
+}
