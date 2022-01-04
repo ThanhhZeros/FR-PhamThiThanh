36813de7c6f6653ce7e8948a26d563003b8270f9
ThanhhZeros <phamthanhnd00@gmail.com>
2022-01-04 13:39:10 +0700

    ex value type+reference

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,11 +4,42 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+		static void DoIncrement(out int param)
+		{
+			param = 6;
+		}
+		static void Main(string[] args)
         {
             Console.Write("\nNhap name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name + " !!");
-        }
+			int a = 2;
+			++a;
+			DoIncrement(out a);
+			Console.WriteLine(a);
+			a++;
+			Console.WriteLine(a);
+			//Console.WriteLine(b);
+			int x;
+			x = 5;
+			int y;
+			y = x;
+			x++;
+			Console.WriteLine(x);
+			Console.WriteLine(y);
+			object c = "abc";
+			object d = "abc";
+			if (c == d)
+			{
+				Console.WriteLine("đúng");
+			}
+			else
+			{
+				Console.WriteLine("sai");
+			}
+			object obj;
+			obj = 100;
+			Console.WriteLine(obj);
+		}
     }
 }
