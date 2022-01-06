adbeda8097136f80e348ccec83f048a8db2a7d4b
ThanhhZeros <phamthanhnd00@gmail.com>
2022-01-06 13:27:59 +0700

    demo lap

---
+++ HelloWorld/Loops.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Loops
+    {
+        public void LapFor()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void LapForeach()
+        {
+            string[] cars = { "Honda", "BMW", "Ford", "Mazda" };
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void LapDoWhile()
+        {
+            int i = 10;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i<20 );
+        }
+    }
+}
