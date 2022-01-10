2e204716d467e2ec377fc5147a7c51316c5e6331
ThanhhZeros <phamthanhnd00@gmail.com>
2022-01-10 10:24:29 +0700

    class and object

---
+++ ClassObject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClassObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape sh = new Shape(5, 7);
+            Console.WriteLine("Dien tich la: " + sh.DienTich());
+        }
+    }
+}
---
+++ ClassObject/Shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    class Shape
+    {
+        float height;
+        float weight;
+        public float DienTich()
+        {
+            return height * weight;
+        }
+        public Shape()
+        {
+            height = 0;
+            weight = 0;
+        }
+        public Shape(float height, float weight)
+        {
+            this.height = height;
+            this.weight = weight;
+        }
+        ~Shape()
+        {
+            Console.WriteLine("Huy "+ height + " va "+ weight);
+        }
+    }
+}
