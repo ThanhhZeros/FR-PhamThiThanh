90b0bf97e04421e727a2533c0b85f48293aafd56
Phạm Thanh <thanh.pham5@ntq-solution.com.vn>
2022-01-14 13:48:58 +0700

    doc ghi text

---
+++ DocGhiFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace DocGhiFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Noi dung trong file: ");
+            /*string a = File.ReadAllText("D:/NTQ_Fresher/DemoCode/FR-PhamThiThanh/DocGhiFile/Text.txt");
+            Console.WriteLine(a);*/
+            string[] a = File.ReadAllLines("D:/NTQ_Fresher/DemoCode/FR-PhamThiThanh/DocGhiFile/Text.txt");
+            for (int i = 0; i < a.Length; ++i)
+            {
+                Console.Write(i + ". ");
+                Console.WriteLine(a[i]);
+            }
+            string noidung = "Ghi file C# khong kho";
+            //File.WriteAllText("D:/NTQ_Fresher/DemoCode/FR-PhamThiThanh/DocGhiFile/Text.txt", noidung);
+            File.AppendAllText("D:/NTQ_Fresher/DemoCode/FR-PhamThiThanh/DocGhiFile/Text.txt", noidung);
+            string b = File.ReadAllText("D:/NTQ_Fresher/DemoCode/FR-PhamThiThanh/DocGhiFile/Text.txt");
+            Console.WriteLine(b);
+        }
+    }
+}
